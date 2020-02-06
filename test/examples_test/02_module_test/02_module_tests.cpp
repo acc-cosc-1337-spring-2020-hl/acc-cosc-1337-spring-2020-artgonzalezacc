8a904f5c3650cb3645e47d29046fc67d06838852
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-05 18:46:49 -0600

    switch statement

--- test/examples_test/02_module_test/02_module_tests.cpp
+++ test/examples_test/02_module_test/02_module_tests.cpp
@@ -2,6 +2,7 @@
 #include "catch.hpp"
 #include "if.h"
 #include "if_else.h"
+#include "switch.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -22,3 +23,12 @@ TEST_CASE("Test get generation funtion")
 	REQUIRE(get_generation(1930) == "Silent Generation");
 }
 
+TEST_CASE("Test menu function") 
+{
+	REQUIRE(menu(1) == "Option 1");
+	REQUIRE(menu(2) == "Option 2");
+	REQUIRE(menu(3) == "Option 3");
+	REQUIRE(menu(4) == "Option 4");
+	REQUIRE(menu(0) == "Invalid Option");
+	REQUIRE(menu(5) == "Invalid Option");
+}
