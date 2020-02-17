bb4f46cde2b92e07e3727ca5a579acf040a393c8
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-17 16:43:40 -0600

    string for range loop

--- src/examples/03_module/04_for_ranged/for_ranged.cpp
+++ src/examples/03_module/04_for_ranged/for_ranged.cpp
@@ -1,8 +1,4 @@
 #include "for_ranged.h"
-#include<iostream>
-#include<vector>
-
-using std::vector;
 
 /*
 Write code for loop_string_w_index that accepts a string parameter.
@@ -15,10 +11,16 @@ e
 s
 t
 */
-
+void loop_string_w_index(const string & str)
+{
+	for (int i = 0; i < str.length(); ++i) 
+	{
+		cout << str[i] << "\n";
+	}
+}
 
 /*
-Write code for loop_string_w_index that accepts a string parameter.
+Write code for loop_string_w_auto that accepts a string parameter.
 The function uses a for ranged loop using auto to iterate and display the characters in the
 string as follows:
 for string test displays
@@ -28,7 +30,13 @@ e
 s
 t
 */
-
+void loop_string_w_auto(const string & str)
+{
+	for (auto s : str) 
+	{
+		cout << s << "\n";
+	}
+}
 
 void loop_vector_w_index() 
 {
