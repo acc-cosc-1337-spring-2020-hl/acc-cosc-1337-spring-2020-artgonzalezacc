d64b04cd408534e17e19e5d0341573c20a2f1cb6
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-19 16:11:56 -0600

    vector for ranged loop

--- src/examples/03_module/06_vectors/vec.cpp
+++ src/examples/03_module/06_vectors/vec.cpp
@@ -1,6 +1,4 @@
-#include "for_ranged.h"
-#include<iostream>
-#include<vector>
+#include "vec.h"
 
 using std::vector;
 
@@ -9,15 +7,26 @@ Write code for function loop_vector_w_index with no parameters.
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
 */
-
-
+void loop_vector_w_index(vector<int> nums)
+{
+	for (int i = 0; i < nums.size(); ++i) 
+	{
+		cout << nums[i] << "\n";
+	}
+}
 
 /*
 Write code for function loop_vector_w_index with a vector of int pass by value parameter.
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows: 9 10 99 5  67
 */
-
+void loop_vector_w_index_ref(vector<int>& nums)
+{
+	for (auto n : nums) 
+	{
+		cout << n << "\n";
+	}
+}
 
 
 /*
@@ -25,6 +34,14 @@ Write code for function loop_vector_w_index with a vector of int pass by referen
 The function creates an int vector with values 9, 10, 99, 5, 67
 and displays the numbers to screen using an indexed for loop as follows : 9 10 99 5  67
 */
+void loop_vector_w_index_const(const vector<int>& nums)
+{
+	for (auto n : nums)
+	{
+		cout << n << "\n";
+	}
+}
+
 
 
 /*
