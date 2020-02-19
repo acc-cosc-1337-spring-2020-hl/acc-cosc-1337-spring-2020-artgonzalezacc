add1a06b056c6dc625a293a196e93a2b4a3eb75a
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-02-19 16:12:06 -0600

    string and vector unit tests

--- src/examples/03_module/03_do_while/main.cpp
+++ src/examples/03_module/03_do_while/main.cpp
@@ -1,8 +1,50 @@
 #include "do_while.h"
+#include<iostream>
+#include<string>
+#include<ctype.h>
+
+using std::cin;
+
+bool is_valid_cin(std::istream& cin) 
+{
+	if (!cin) 
+	{
+		cin.clear();
+		cin.ignore(100, '\n');
+		return false;
+	}
+
+	return true;
+}
+
+bool is_alpha_string(std::string& str) 
+{
+	for (auto s : str) 
+	{
+		if (isdigit(s)) 
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
 
 int main() 
 {	
-	prompt_user();
+	//prompt_user();
+	std::string name;
+	
+	double num;
+	do 
+	{
+		//std::cout << "Enter name: ";
+		//std::cin >> name;
+		std::cout << "Enter num: ";
+		std::cin >> num;
+
+	} while (!is_valid_cin(cin));
+	// while (!is_alpha_string(name));
 	
 	return 0;
 }
--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -2,6 +2,10 @@
 #include "catch.hpp"
 #include "while.h"
 #include "value_ref.h"
+#include "for_ranged.h"
+#include <string>
+
+using std::string;
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -24,3 +28,18 @@ TEST_CASE("Test value and ref function")
 	REQUIRE(num2 == 50);
 }
 
+TEST_CASE("Test for ranged loop by value") 
+{
+	string name = "Joe";
+	loop_string_w_auto(name);
+
+	REQUIRE(name == "Joe");
+}
+
+TEST_CASE("Test for ranged loop by reference")
+{
+	string name = "Joe";
+	loop_string_w_auto_ref(name);
+
+	REQUIRE(name == "zzz");
+}
