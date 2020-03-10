2d530bf79d6aa1ef37edf12e2368acbacbd3d3c3
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-03-10 17:28:57 -0500

    bank account

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -1,4 +1,4 @@
-#include "bank_account.h"
+#include "checking_account.h"
 #include<iostream>
 #include<vector>
 
@@ -6,8 +6,8 @@ using std::cout; using std::vector;
 
 int main()
 {
-
-	BankAccount a(50), b(10);
+	CheckingAccount c;
+	CheckingAccount a(50), b(10);
 	//BankAccount c = a + b;
 	display_balance(a);
 	std::cin >> a;
