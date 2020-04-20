71e30eb21abb746994daa789346afbf8c5d380f5
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-04-20 17:51:45 -0500

    ref and pointers

--- src/examples/11_module/02_dynamic_memory/main.cpp
+++ src/examples/11_module/02_dynamic_memory/main.cpp
@@ -1,5 +1,17 @@
+#include<iostream>
+
+using std::cout;
+
 int main() 
 {
+	int* ptr_num = new int(5);//declare and acquire new memory(heap)
+
+	cout << &ptr_num << "\n";//displays address where ptr_num is stored
+	cout << ptr_num << "\n";//address where 5 is stored
+	cout << *ptr_num;
+
+	delete ptr_num;
+	ptr_num = nullptr;
 	
 	return 0;
 }
