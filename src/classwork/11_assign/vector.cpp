88ba425bcf266a2072af900e30ef93ee1a64075d
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-04-22 18:07:17 -0500

    my vector-destructor

--- src/classwork/11_assign/vector.cpp
+++ src/classwork/11_assign/vector.cpp
@@ -1,8 +1,10 @@
 #include "vector.h"
+#include<iostream>
 
 Vector::Vector(size_t sz)
 	:size{sz}, nums{new int[sz]}
 {
+	std::cout << "allocate memory\n";
 	for (size_t i = 0; i < sz; ++i) 
 	{
 		nums[i] = 0;
@@ -16,4 +18,15 @@ Vector::Vector(const Vector & v)
 	{
 		nums[i] = v[i];
 	}
+}
+
+Vector::~Vector() 
+{
+	std::cout << "release memory\n\n";
+	delete[] nums;
+}
+
+void use_vector()
+{
+	Vector v(3);	
 }
