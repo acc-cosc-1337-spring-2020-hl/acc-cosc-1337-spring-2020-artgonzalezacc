2bc9e0fe0a29e038ae56cf86accf5605042397e6
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-05-07 19:04:37 -0500

    stack char arrays

--- src/examples/12_module/06_arrays_char/arrays_char.cpp
+++ src/examples/12_module/06_arrays_char/arrays_char.cpp
@@ -1,1 +1,26 @@
 //cpp
+#include<iostream>
+#include "arrays_char.h"
+
+void char_array() 
+{
+	const int SIZE = 5;
+	char name[SIZE] = "Mary";
+	name[4] = '\0';
+	std::cout << name << "\n\n";
+
+	for (int i = 0; name[i] != '\0'; ++i) 
+	{
+		std::cout << name[i] << "\n";
+	}
+
+	
+}
+
+void char_array_no_size() 
+{
+	char name[] = "Mary";
+	std::cout << name << "\n\n";
+
+	
+}
--- src/examples/12_module/06_arrays_char/main.cpp
+++ src/examples/12_module/06_arrays_char/main.cpp
@@ -1,8 +1,8 @@
-
+#include "arrays_char.h"
 
 int main()
 {
-	
+	char_array_no_size();
 
 	return 0;
 };
